5fde6659783a13ace3bc30884c2a4ad67fb5f28c
Blokhin Dmitriy <sv.dblokhin@gmail.com>
2018-01-16 19:29:11 +1000

    intro secp256 c-binding

---
+++ src/secp256k1zkp/pedersen.go
@@ -0,0 +1,23 @@
+// Copyright 2018 The Gringo Developers. All rights reserved.
+// Use of this source code is governed by a GNU GENERAL PUBLIC LICENSE v3
+// license that can be found in the LICENSE file.
+
+package secp256k1zkp
+
+import (
+	"io"
+)
+
+type Commitment []byte
+
+// Bytes implements p2p Message interface
+func (c *Commitment) Bytes() []byte {
+	return *c
+}
+
+// Read implements p2p Message interface
+func (c *Commitment) Read(r io.Reader) error {
+	_, err := io.ReadFull(r, *c)
+
+	return err
+}
