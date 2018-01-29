08d8a9400d154aa7ff674e2c3c7084860e2fe404
Blokhin Dmitriy <sv.dblokhin@gmail.com>
2018-01-29 11:24:21 +1000

    complete proof struct & methods - ToDifficulty() - Hash() - Bytes()

--- src/consensus/proof.go
+++ src/consensus/proof.go
@@ -5,6 +5,11 @@
 package consensus
 
 import (
+	"bytes"
+	"github.com/sirupsen/logrus"
+	"encoding/binary"
+	"errors"
+	"golang.org/x/crypto/blake2b"
 )
 
 // RangeProof of work
@@ -14,6 +19,41 @@ type Proof struct  {
 
 	// The proof size
  	ProofSize uint
+}
+
+// Validate validates the pow
+func (p *Proof) Validate() error {
+	return nil
+}
+
+// ToDifficulty converts the proof to a proof-of-work Target so they can be compared.
+// Hashes the Cuckoo Proof data.
+func (p *Proof) ToDifficulty() Difficulty {
+	return MinimumDifficulty.FromHash(p.Hash())
+}
+
+// Hash returns hash of content pow
+func (p *Proof) Hash() []byte {
+	hash := blake2b.Sum256(p.Bytes())
+	return hash[:]
+}
+
+// Bytes returns binary []byte
+func (p *Proof) Bytes() []byte {
+	buff := new(bytes.Buffer)
+
+	// Write POW
+	if uint32(p.ProofSize) != ProofSize || uint32(len(p.Nonces)) != ProofSize {
+		logrus.Fatal(errors.New("invalid proof len"))
+	}
+
+	for i := 0; i < int(ProofSize); i++ {
+		if err := binary.Write(buff, binary.BigEndian, p.Nonces[i]); err != nil {
+			logrus.Fatal(err)
+		}
+	}
+
+	return buff.Bytes()
 }
 
 func NewProof(nonces []uint32) Proof {
