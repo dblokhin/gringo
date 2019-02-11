8d15a3ea475d9d8c5faf80883ddd9185cea14b67
John Yossarian <jyoss22@outlook.com>
2019-02-11 12:06:55 +0800

    chain: Add floonet genesis block

--- chain/chain_test.go
+++ chain/chain_test.go
@@ -15,3 +15,13 @@ func TestGenesisHash(t *testing.T) {
 			hash, expected, Testnet4.Bytes())
 	}
 }
+
+func TestFloonetGenesisHash(t *testing.T) {
+	hash := Floonet.Hash()
+	expected, _ := hex.DecodeString("edc758c1370d43e1d733f70f58cf187c3be8242830429b1676b89fd91ccf2dab")
+
+	if bytes.Compare(hash, expected) != 0 {
+		t.Errorf("Genesis hash was %v wanted %x. Content:\n%x\n",
+			hash, expected, Floonet.Bytes())
+	}
+}
